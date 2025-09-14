337c4b0a956a4795a49fc8be5f96cbf5f8213bc4
Roman Mitrofanov <mitrofanovan@mail.ru>
2025-09-14 11:07:15 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+if (document.documentElement.clientWidth > 1028) {
+  const examplesButtons = document.querySelector(".info__buttons");
+  if (examplesButtons) {
+    examplesButtons.children[1].addEventListener("mouseenter", function (e) {
+      examplesButtons.children[0].classList.remove("button_blue");
+    });
+    examplesButtons.children[1].addEventListener("mouseleave", function (e) {
+      examplesButtons.children[0].classList.add("button_blue");
+    });
+  }
+}
