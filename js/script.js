3ef6039d2a2d649b32e6e9fcf0d2931145e877d3
Roman Mitrofanov <mitrofanovan@mail.ru>
2025-09-17 21:16:22 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -30,34 +30,36 @@ if (links) {
 
 emailjs.init("X0ywkiXLz5gT5ZUvz");
 const form = document.getElementById("contact-form");
-const message = document.querySelector(".message");
-const inputName = document.getElementById("name");
-const inputEmail = document.getElementById("email");
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+if (form) {
+  const message = document.querySelector(".message");
+  const inputName = document.getElementById("name");
+  const inputEmail = document.getElementById("email");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  form.classList.add("_active");
-  emailjs.sendForm("service_9ksdmzg", "template_wakndhn", this).then(
-    function () {
-      // alert("Сообщение отправлено!");
-      form.classList.remove("_active");
-      form.reset();
-      message.innerHTML = `Данные успешно отправлены!`;
-      message.classList.add("_active");
-      setTimeout(() => {
-        message.classList.remove("_active");
-      }, 4000);
-    },
-    function (error) {
-      // alert("Ошибка: " + JSON.stringify(error));
-      form.classList.remove("_active");
-      message.innerHTML = `Ошибка: ${JSON.stringify(error)}`;
+    form.classList.add("_active");
+    emailjs.sendForm("service_9ksdmzg", "template_wakndhn", this).then(
+      function () {
+        // alert("Сообщение отправлено!");
+        form.classList.remove("_active");
+        form.reset();
+        message.innerHTML = `Данные успешно отправлены!`;
+        message.classList.add("_active");
+        setTimeout(() => {
+          message.classList.remove("_active");
+        }, 4000);
+      },
+      function (error) {
+        // alert("Ошибка: " + JSON.stringify(error));
+        form.classList.remove("_active");
+        message.innerHTML = `Ошибка: ${JSON.stringify(error)}`;
 
-      message.classList.add("_active");
-      setTimeout(() => {
-        message.classList.remove("_active");
-      }, 4000);
-    }
-  );
-});
+        message.classList.add("_active");
+        setTimeout(() => {
+          message.classList.remove("_active");
+        }, 4000);
+      }
+    );
+  });
+}
