e9e6a148d72b0df14e5fc7f23d52b92457e0aa6b
Roman Mitrofanov <mitrofanovan@mail.ru>
2025-09-18 16:31:14 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+if (typeof WOW === "function" && document.documentElement.clientWidth >= 1022) {
+  new WOW().init();
+}
+
 if (document.documentElement.clientWidth >= 1022) {
   const examplesButtons = document.querySelector(".info__buttons");
   if (examplesButtons) {
@@ -13,14 +17,10 @@ if (document.documentElement.clientWidth >= 1022) {
 }
 
 const links = document.querySelectorAll("a.anchor");
-console.log(links);
 if (links) {
   links.forEach((element) => {
     element.addEventListener("click", function (e) {
       e.preventDefault();
-      console.log(
-        document.querySelector(`#${element.href.toString().split("#")[1]}`)
-      );
       document
         .querySelector(`#${element.href.toString().split("#")[1]}`)
         .scrollIntoView({ behavior: "smooth" });
@@ -38,28 +38,69 @@ if (form) {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    form.classList.add("_active");
-    emailjs.sendForm("service_9ksdmzg", "template_wakndhn", this).then(
-      function () {
-        // alert("Сообщение отправлено!");
-        form.classList.remove("_active");
-        form.reset();
-        message.innerHTML = `Данные успешно отправлены!`;
-        message.classList.add("_active");
-        setTimeout(() => {
-          message.classList.remove("_active");
-        }, 4000);
-      },
-      function (error) {
-        // alert("Ошибка: " + JSON.stringify(error));
-        form.classList.remove("_active");
-        message.innerHTML = `Ошибка: ${JSON.stringify(error)}`;
+    if (formValidate(form)) {
+      form.classList.add("_active");
+      emailjs.sendForm("service_9ksdmzg", "template_wakndhn", this).then(
+        function () {
+          form.classList.remove("_active");
+          form.reset();
+          message.innerHTML = `Данные успешно отправлены!`;
+          message.classList.add("_active");
+          setTimeout(() => {
+            message.classList.remove("_active");
+          }, 4000);
+        },
+        function (error) {
+          form.classList.remove("_active");
+          message.innerHTML = `Ошибка: ${JSON.stringify(error)}`;
 
-        message.classList.add("_active");
-        setTimeout(() => {
-          message.classList.remove("_active");
-        }, 4000);
-      }
-    );
+          message.classList.add("_active");
+          setTimeout(() => {
+            message.classList.remove("_active");
+          }, 4000);
+        }
+      );
+    }
   });
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll("._req");
+    formReq.forEach((item) => {
+      item.addEventListener("focus", function (e) {
+        console.log(1);
+        item.parentElement.classList.remove("_error");
+      });
+      console.log(item);
+      formRemoveError(item);
+      if (item.classList.contains("_email")) {
+        if (emailTest(item)) {
+          formAddError(item);
+          error++;
+        }
+      } else if (
+        item.getAttribute("type") === "checkbox" &&
+        item.checked === false
+      ) {
+        formAddError(item);
+        error++;
+      } else {
+        if (item.value === "") {
+          formAddError(item);
+          error++;
+        }
+      }
+    });
+    return !error;
+  }
+  function formAddError(input) {
+    input.parentElement.classList.add("_error");
+    input.classList.add("_error");
+  }
+  function formRemoveError(input) {
+    input.parentElement.classList.remove("_error");
+    input.classList.remove("_error");
+  }
+  function emailTest(input) {
+    return !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value);
+  }
 }
